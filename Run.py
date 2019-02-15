21067238f65a13f8e3f8434fad19d4ea97fef37a
Abdurahman Omoruyi <45211844+ObbieOnOblivion@users.noreply.github.com>
2019-02-15 17:34:26 -0500

    Run application
    This runs the other files

---
+++ Run.py
@@ -0,0 +1,8 @@
+# import toyota_supra_v2
+import toyota_supra_v3
+
+# if __name__ == '__main__':
+#     toyota_supra_v2.OBBIE.use_screen()
+
+if __name__ == '__main__':
+    toyota_supra_v3.OBBIE.use_screen()
