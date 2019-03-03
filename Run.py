772885403d7f28eb5f51f9d06ce637c46e55d30d
Abdurahman Omoruyi <45211844+ObbieOnOblivion@users.noreply.github.com>
2019-03-03 09:34:04 -0500

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,8 +1,7 @@
-# import toyota_supra_v2
 import toyota_supra_v3
 
 # if __name__ == '__main__':
-#     toyota_supra_v2.OBBIE.use_screen()
+#     toyota_supra_v3.OBBIE.use_screen()
 
 if __name__ == '__main__':
-    toyota_supra_v3.OBBIE.use_screen()
+    toyota_supra_v3.OBBIE.what_we_do()
